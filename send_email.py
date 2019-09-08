e7a5557731b6063236de325ba1f81faa01f0c1c5
Desmond Yeoh <desmondyeoh11@gmail.com>
2019-09-08 12:08:54 +0800

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -3,12 +3,18 @@ send_email method
 """
 
 def send_email(username, password, sendto, subject, text):
-    print("Sending Email to %s..." % sendto)
-    import smtplib
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.starttls()
-    server.login(username, password)
-    msg = 'Subject: {}\n\n{}'.format(subject, text)
-    server.sendmail(username, sendto, msg)
-    server.quit()
-    print("Email sent!")
+    for i in range(3):
+        try:
+            print("Sending Email to {} (trial {})...".format(sendto, i+1))
+            import smtplib
+            server = smtplib.SMTP('smtp.gmail.com', 587)
+            server.starttls()
+            server.login(username, password)
+            msg = 'Subject: {}\n\n{}'.format(subject, text)
+            server.sendmail(username, sendto, msg)
+            server.quit()
+            print("Email sent!")
+            break
+        except Exception as e:
+            print("Failed to send email due to Exception:")
+            print(e)
